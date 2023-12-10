4d7c3786ededac7ee18386e46db969166da9ed7d
Mst-Sarmin-Zahan <141745771+Mst-Sarmin-Zahan@users.noreply.github.com>
2023-12-10 22:35:12 +0600

    Update api.js

--- full-fledged e-commerce website/src/routes/api.js
+++ full-fledged e-commerce website/src/routes/api.js
@@ -1,23 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-const studentController = require("../controllers/studentController");
-
-
-// create
-router.post("/CreateData",studentController.createStudentList);
-
-//Read 
-router.get("/ReadData",studentController.ReadStudentList);
-
-//Read By ID
-router.get("/ReadByID/:id", studentController.ReadByID);
-
-//Delete 
-router.delete("/deleteData/:id", studentController.deleteStudentList);
-
-//Update
-router.post("/updateData/:id", studentController.updateStudentList);
 
 
 
